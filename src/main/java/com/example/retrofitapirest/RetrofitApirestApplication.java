8b3088c50d0f412791d8e14e7163886c48dbe124
Farkhat <farqat@mail.ru>
2022-10-12 12:56:25 +0600

    Init commit

---
+++ src/main/java/com/example/retrofitapirest/RetrofitApirestApplication.java
@@ -0,0 +1,13 @@
+package com.example.retrofitapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrofitApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetrofitApirestApplication.class, args);
+    }
+
+}
